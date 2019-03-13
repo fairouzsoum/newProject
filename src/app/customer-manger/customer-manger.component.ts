908474d972c894c95d90cd0919783f5a423e7019
unknown <n.karce@pharmainvest.dz>
2019-03-13 16:21:40 +0100

    display the customers

--- src/app/customer-manger/customer-manger.component.ts
+++ src/app/customer-manger/customer-manger.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomersService } from '../customers.service';
 import { IUser } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-manger',
@@ -9,18 +10,18 @@ import { IUser } from '../models/user.model';
 })
 export class CustomerMangerComponent implements OnInit {
   // tslint:disable-next-line: variable-name
-  constructor(private _serviceCustomersManger: CustomersService) {}
+  constructor(private _serviceCustomersManger: CustomersService, private _router: Router) {}
   public customers;
   public listCust: IUser[];
   public messageError: string;
   ngOnInit() {}
   public displayCustomers() {
+    this._router.navigate(['customerManger']);
     this.customers = this._serviceCustomersManger
       .getCustomers()
       .subscribe(
         res => (this.listCust = res),
         err => (this.messageError = err)
       );
-    console.log(this._serviceCustomersManger.getCustomers());
   }
 }
