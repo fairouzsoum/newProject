b3c0890a2cac48b7c9783b30269f433c78072fe7
unknown <f.salem@pharmainvest.dz>
2019-03-13 11:11:49 +0100

    add nav bar for customerManger

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerMangerComponent } from './customer-manger/customer-manger.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/customerManger', pathMatch: 'full' },
+  { path: 'customerManger', component: CustomerMangerComponent }
+  /*  { path: '', component: 'customerManger'},
+  { path: '', component: 'customerManger'},
+  { path: '', component: 'customerManger'}, */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
