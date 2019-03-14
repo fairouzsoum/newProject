655c4c606323700ea969b09bc2862fab17991f90
unknown <n.karce@pharmainvest.dz>
2019-03-14 10:34:40 +0100

    add new folder of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import { CustomerMangerComponent } from './customer-manger/customer-manger.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FundamentalNgxModule } from 'fundamental-ngx';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ModalComponent } from './core/modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, CustomerMangerComponent],
+  declarations: [AppComponent, CustomerMangerComponent, NavbarComponent, AboutComponent, ModalComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
